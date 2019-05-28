3add4497ff54a6e8b16b12944304558bfd6239c6
Wazir Robinson <robinson82425@gmail.com>
2019-05-28 18:49:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return animal;
 }
-const two = 2
-function add2(n) {
-
-  return n + two;
-
-  // Feel free to move things around!
-
-}
+// const two = 2
+// function add2(n) {
 //
-// var funkyFunction = function(theFunk) {
+//   return n + two;
 //
-//   return function() {
+//   // Feel free to move things around!
 //
-//     return "FUNKY!";
-//   }
 // }
+//
+var funkyFunction = function(theFunk) {
+
+  return function() {
+
+    return "FUNKY!";
+  }
+}
 // //
 // // // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // // // NOTE: you only need to modify the code below this line.
