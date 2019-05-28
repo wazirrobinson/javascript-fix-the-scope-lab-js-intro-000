d4278036dae7671a825853d78577969464984360
Wazir Robinson <robinson82425@gmail.com>
2019-05-28 18:47:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ var animal = 'dog'
 function myAnimal() {
   return animal;
 }
-var yourAnimal = cat;
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal;
-}
-const two = 2
-function add2(n) {
-
-  return n + two;
-
-  // Feel free to move things around!
-
-}
-
-var funkyFunction = function(theFunk) {
-
-  return function() {
-
-    return "FUNKY!";
-  }
-}
+// var yourAnimal = cat;
+// function yourAnimal() {
+//   // How can we make sure that this function
+//   // and the above function both pass?
+//   // P.S.: You can't just hard-code 'cat' below
+//   return animal;
+// }
+// const two = 2
+// function add2(n) {
 //
-// // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction;
+//   return n + two;
+//
+//   // Feel free to move things around!
+//
+// }
+//
+// var funkyFunction = function(theFunk) {
+//
+//   return function() {
+//
+//     return "FUNKY!";
+//   }
+// }
+// //
+// // // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// // // NOTE: you only need to modify the code below this line.
+// var theFunk = funkyFunction;
